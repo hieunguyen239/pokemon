e2090ef0f491754006510f08ef469ff2958941d4
Hieu Nguyen <hieu.nguyenminh@gforces.co.uk>
2022-10-17 14:51:01 +0700

    back btn

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,6 +1,11 @@
 <template>
   <Transition>
     <div class="detail" v-if="show">
+      <div class="p-4">
+        <router-link :to="`/listing/`">
+          &lt; Back to listing
+        </router-link>
+      </div>
       <figure class="text-center">
         <img :src="pokemon.image" alt="" srcset="" class="rounded-full inline-block" />
 
@@ -73,7 +78,8 @@ const elementColors = computed(() => ({
   grass: 'green',
   poison: 'purple',
   fire: 'red',
-  default: '#ccc',
+  default: '#ddd',
+  water: 'blue'
 }));
 
 const baseStats = computed(() => {
@@ -120,7 +126,8 @@ const elements = computed(() => {
 function mapElementColor(color) {
   const c = elementColors.value[color];
 
-  return c || elementColors['default'];
+
+  return c || elementColors.value['default'];
 }
 
 onMounted(async () => {
