badd22989e9292e179f8d659f4d8d2f7d5c4a728
hienbuigforces <80395841+hienbuigforces@users.noreply.github.com>
2022-10-17 14:03:37 +0700

    Testing - Update 1st test case

--- cypress/support/page-objects/basePg.js
+++ cypress/support/page-objects/basePg.js
@@ -1,4 +1,8 @@
+/// <reference types="cypress" />
+
 export class BasePage {
-
+  initIntercept() {
+    cy.intercept("/api/v2/pokemon").as("pokemon");
+    cy.intercept("/api/v2/pokemon/*").as("pokemon-details");
+  }
 }
-
