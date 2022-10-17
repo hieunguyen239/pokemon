aca3668a07fded1e7ecfa18a3c4af1c4573bd4c5
Hieu Nguyen <hieu.nguyenminh@gforces.co.uk>
2022-10-17 13:37:49 +0700

    detail page

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,25 +5,25 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/listing',
-      name: 'listing',
+      path: "/listing",
+      name: "listing",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('@/components/PokemonListing.vue'),
+      component: () => import("@/components/PokemonListing.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/detail",
+      name: "detail",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import("../views/DetailView.vue"),
     },
   ],
 });
