0d525b8034d3f9899666d6dbf019ce128d39acf6
KhoiTruong <khoi.truongsu@gforces.co.uk>
2022-10-17 12:41:02 +0700

    add pokemon card

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="rounded-2xl border-solid shadow-xl pokemon-card bg-gray-50 m-3">
+    <div class="">
+      <img class="" src="../../public/border-solid.png" alt="" />
+    </div>
+    <div>
+        <p class="text-2xl text-black text-center m-3">bulbasaur</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style>
+.pokemon-card {
+  /* width: 50%; */
+}
+img {
+  width: 100%;
+}
+</style>
